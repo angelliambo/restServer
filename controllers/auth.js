909d1a293fc0da1d989440de1cfe36fcf375527f
Angel Liambo <aliambo@jobint.com>
2022-10-13 23:34:43 -0300

    login: correo password y estado OK

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,46 @@
 const { response } = require("express");
+const bcryptjs = require("bcryptjs");
+const Usuario = require("../models/usuario.js");
 
-const login = (req, res = response) => {
-  res.json({
-    msg: "login OK",
-  });
+const login = async (req, res = response) => {
+  const { correo, password } = req.body;
+
+  try {
+    // Verificar email
+    const usuario = await Usuario.findOne({ correo });
+
+    if (!usuario) {
+      return res.status(400).json({
+        msg: "Usuario/password no existe -correo",
+      });
+    }
+
+    // Verificar usuario activo
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: "Usuario/password no existe -estado",
+      });
+    }
+
+    // Verificar password correcto
+    const passwordIsValid = bcryptjs.compareSync(password, usuario.password);
+
+    if (!passwordIsValid) {
+      return res.status(400).json({
+        msg: "Usuario/password no existe -password",
+      });
+    }
+    // Generar JWT
+
+    res.json({
+      msg: "login OK",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: "Error algo salio mal",
+      error,
+    });
+  }
 };
 
 module.exports = { login };
